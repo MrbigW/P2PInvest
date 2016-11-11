e22f395dea6ea39bab1b2b1ee9ba4a0169202159
[Window Title] <1024057635@qq.com>
2016-11-11 19:30:55 +0800

    Initial commit

---
+++ app/src/main/java/com/atsgg/p2pinvest/fragment/MeFragment.java
@@ -0,0 +1,58 @@
+package com.atsgg.p2pinvest.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.atsgg.p2pinvest.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by MrbigW on 2016/11/11.
+ * weChat:1024057635
+ * GitHub:MrbigW
+ * Usage: -.-
+ * -------------------=.=------------------------
+ */
+
+public class MeFragment extends Fragment {
+
+    @BindView(R.id.iv_top_back)
+    ImageView ivTopBack;
+    @BindView(R.id.tv_back_title)
+    TextView tvBackTitle;
+    @BindView(R.id.iv_top_setting)
+    ImageView ivTopSetting;
+    private Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(mContext, R.layout.fragment_me, null);
+        ButterKnife.bind(this, view);
+
+        initTitle();
+
+        return view;
+    }
+
+    private void initTitle() {
+        ivTopBack.setVisibility(View.INVISIBLE);
+        tvBackTitle.setText("我的资产");
+        ivTopSetting.setVisibility(View.INVISIBLE);
+    }
+}
